efcc970cd12ce05627043f3890cb5fcd89bc708b
devachref <bennadjiachref5@gmail.com>
2023-11-10 09:34:48 -0800

    send

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,17 +9,3 @@ def roman_to_int(roman_string):
         num = digits[r]
         total += num if total < num * 5 else -num
     return total
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
