e005fa4a49869bccef01a23e2fb24dc3994fb0da
devachref <bennadjiachref5@gmail.com>
2023-11-20 11:29:22 -0800

    send

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         r = None
     finally:
-        print("Inside result: ()".format(r))
+        print("Inside result: {}".format(r))
         return r
