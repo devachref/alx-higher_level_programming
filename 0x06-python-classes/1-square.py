53b8ff75052c91db855da603618450e23202747b
devachref <bennadjiachref5@gmail.com>
2023-11-22 12:27:12 -0800

    send

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-
-"""Square module."""
+"""Square class definition"""
 
 
 class Square:
-    """Defines a square."""
-    
+    """Represents a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size):
-        """Constructor.
+        """Initializes a square
 
         Args:
-            size(int): length of a side of the square.
+            size (int): size of a side of the square
+
+        Returns: None
         """
         self.__size = size
