1e9ccdd46738feff1d662267a03549fa2254e38b
devachref <bennadjiachref5@gmail.com>
2023-12-05 20:29:06 +0100

    hello

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-def save_to_json_file(my_obj, filename):
-    from json import dumps
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.write(dumps(my_obj))
-        f.close()
+'''task 7 module'''
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+arglist = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
+    old_data = []
+
+old_data.extend(arglist)
+save_to_json_file(old_data, 'add_item.json')
