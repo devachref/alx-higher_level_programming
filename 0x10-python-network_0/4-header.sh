608681760aeefe4b024c2bc249fe8297630bfa28
devachref <bennadjiachref5@gmail.com>
2024-03-06 00:39:02 +0100

    send

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# Bash script that takes in a URL as an argument
-curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
+
+# Check if the number of arguments is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Set the URL from the command-line argument
+url="$1"
+
+# Use curl to send a GET request with the specified header
+curl -sH "X-School-User-Id: 98" "$url"
+
